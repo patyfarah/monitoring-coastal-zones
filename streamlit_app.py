f3466626b28977fe065a25330dbe0bc9f8bc2095
Patty Farah <118547769+patyfarah@users.noreply.github.com>
2025-04-24 21:22:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ with col1:
 # Map on the right
 with col2:
     st.subheader("Good Environmental Status")
-    Map = geemap.Map()
+    Map = geemap.Map(measure_ctrl=False,data_ctrl=False,toolbar_ctrl=False)
 
     # Filter by country
     countries = ee.FeatureCollection("USDOS/LSIB_SIMPLE/2017")
