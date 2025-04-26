2844e7e4f2cc27bdafe2dc2cdb8a78f442b872a5
Patty Farah <118547769+patyfarah@users.noreply.github.com>
2025-04-26 11:15:46 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -138,7 +138,6 @@ with col2:
     # Add layers
     Map.addLayer(ndvi_mean, ndviVis, 'Mean NDVI',shown=False)
     Map.addLayer(lst_mean, lstVis, 'Mean LST',shown=False)
-    Map.addLayer(outer_band, {}, 'Outer zone',shown=False)
     Map.addLayer(filtered.style(**{
     "color": "black",
     "fillColor": "00000000",  # Transparent fill
