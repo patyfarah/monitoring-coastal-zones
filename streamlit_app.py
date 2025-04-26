bea070305a31ef0c089b89279754c9117947d503
Patty Farah <118547769+patyfarah@users.noreply.github.com>
2025-04-26 10:46:54 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,8 +76,8 @@ with col1:
         .filterBounds(filtered)
         .filterDate(start_date, end_date)
     )
-    ndvi_mean = ndvi.mean().clip(filtered)
-    lst_mean = lst.mean().clip(filtered)
+    ndvi_mean = ndvi.mean().clip(outer_band)
+    lst_mean = lst.mean().clip(outer_band)
 
    # Define region of interest
     region = filtered.geometry()
