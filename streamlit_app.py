f17e9692a752c5760b49efdbba81ba2f5294353a
Patty Farah <118547769+patyfarah@users.noreply.github.com>
2025-04-29 19:17:33 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -181,7 +181,7 @@ with col2:
     st.markdown("Mean Temperature Statistics (°C)")
     stats = lst_mean.reduceRegion(
     reducer=ee.Reducer.minMax(),
-    geometry=outer_band.geometry(),
+    geometry=region.geometry(),
     scale=1000,
     maxPixels=1e13
     )
